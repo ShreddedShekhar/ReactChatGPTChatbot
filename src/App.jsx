13eb1e3f0edcedde94a99afb1575b0c96117e760
Himanshu Shekhar <himanshu.shekhar@clear.in>
2023-08-23 18:34:19 +0530

    feat: init commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
-import { useState } from 'react'
+import { useRef, useState } from 'react'
 import './App.css'
 import '@chatscope/chat-ui-kit-styles/dist/default/styles.min.css';
 import { MainContainer, ChatContainer, MessageList, Message, MessageInput, TypingIndicator } from '@chatscope/chat-ui-kit-react';
+import { Box, Modal, Typography } from '@mui/material';
 
-const API_KEY = "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";
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  p: 4,
+};
+const API_KEY = "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";
 // "Explain things like you would to a 10 year old learning how to code."
 const systemMessage = { //  Explain things like you're talking to a software professional with 5 years of experience.
   "role": "system", "content": "Explain things like you're talking to a software professional with 2 years of experience."
@@ -18,7 +30,7 @@ function App() {
     }
   ]);
   const [isTyping, setIsTyping] = useState(false);
-
+  const counter = useRef(1);
   const handleSend = async (message) => {
     const newMessage = {
       message,
@@ -27,13 +39,19 @@ function App() {
     };
 
     const newMessages = [...messages, newMessage];
-    
+
     setMessages(newMessages);
 
     // Initial system message to determine ChatGPT functionality
     // How it responds, how it talks, etc.
     setIsTyping(true);
-    await processMessageToChatGPT(newMessages);
+    if (!(counter.current % 5 === 0)) {
+      await processMessageToChatGPT(newMessages);
+      counter.current = counter.current + 1;
+    }
+    else {
+      setModal(true)
+    }
   };
 
   async function processMessageToChatGPT(chatMessages) { // messages is an array of messages
@@ -48,7 +66,7 @@ function App() {
       } else {
         role = "user";
       }
-      return { role: role, content: messageObject.message}
+      return { role: role, content: messageObject.message }
     });
 
 
@@ -63,46 +81,67 @@ function App() {
       ]
     }
 
-    await fetch("https://api.openai.com/v1/chat/completions", 
-    {
-      method: "POST",
-      headers: {
-        "Authorization": "Bearer " + API_KEY,
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify(apiRequestBody)
-    }).then((data) => {
-      return data.json();
-    }).then((data) => {
-      console.log(data);
-      setMessages([...chatMessages, {
-        message: data.choices[0].message.content,
-        sender: "ChatGPT"
-      }]);
-      setIsTyping(false);
-    });
+    await fetch("https://api.openai.com/v1/chat/completions",
+      {
+        method: "POST",
+        headers: {
+          "Authorization": "Bearer " + API_KEY,
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify(apiRequestBody)
+      }).then((data) => {
+        return data.json();
+      }).then((data) => {
+        console.log(data);
+        setMessages([...chatMessages, {
+          message: data.choices[0].message.content,
+          sender: "ChatGPT"
+        }]);
+        setIsTyping(false);
+      });
   }
-
+  const [modal, setModal] = useState(false)
+  const handleClose = ()=>{
+    setModal(false)
+    counter.current = 0;
+  }
   return (
     <div className="App">
-      <div style={{ position:"relative", height: "800px", width: "700px"  }}>
+      <div className='header' >
+        <img style={{ display: 'flex', height: 'auto' }} src='https://i.gadgets360cdn.com/large/cleartax_mascot_1491905141772.jpg' height={100} width={100} />
+        <span>Hey Buddy! Let me help you with your querries</span>
+      </div>
+      <div style={{ position: "relative", height: "800px", width: "700px" }}>
         <MainContainer>
-          <ChatContainer>       
-            <MessageList 
-              scrollBehavior="smooth" 
-              typingIndicator={isTyping ? <TypingIndicator content="ChatGPT is typing" /> : null}
+          <ChatContainer>
+            <MessageList
+              scrollBehavior="smooth"
+              typingIndicator={isTyping ? <TypingIndicator content="Sewak likh raha hai" /> : null}
             >
               {messages.map((message, i) => {
                 console.log(message)
                 return <Message key={i} model={message} />
               })}
             </MessageList>
-            <MessageInput placeholder="Type message here" onSend={handleSend} />        
+            <MessageInput placeholder="Haan ji Sir Bataaiyee" onSend={handleSend} />
           </ChatContainer>
         </MainContainer>
       </div>
+      <Modal
+        open={modal}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <Typography id="modal-modal-title" variant="h6" component="h2">
+          Hello! I hope the solution I provided was helpful.? If not you can call us on <a href="tel:123-456-7890">+91-9878582717</a> 
+          </Typography>
+        </Box>
+      </Modal>
     </div>
   )
 }
 
 export default App
+
